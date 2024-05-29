0eba5a002dca06d5cc6c2e8d132e282c864c460e
iamshecktar1996 <iamshecktar1996@gmail.com>
2024-05-29 20:30:23 +0000

    instantiate babel object

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+This Flask app is configured to support English and French using Flask-Babel
+for i18n.
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    Render the index.html template with a welcome message.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
