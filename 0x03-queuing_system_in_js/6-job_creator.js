2ff35e42603ffc68750a74e2a99a29e91b3e17c4
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-26 21:36:17 +0000

    Create a Redic client

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is the message to be sent'
+};
+
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) console.log(`Notification job created: ${job.id}`);
+  });
+
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', (errorMessage) => {
+  console.log('Notification job failed');
+});
